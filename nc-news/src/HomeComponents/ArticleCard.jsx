cc4a4b7a6e5b61cc17b5c82e56992cf66c891f41
Millie-Jay <millicentbowers@gmail.com>
2024-08-07 13:32:46 +0100

    single article page and link and some editing of the mobile side of CSS

--- nc-news/src/HomeComponents/ArticleCard.jsx
+++ nc-news/src/HomeComponents/ArticleCard.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ArticleCard(props) {
-  const { article } = props;
-
+export default function ArticleCard({ article }) {
   return (
-    <Link to={"/articles/${article.article_id}"}>
+    <Link to={`/articles/${article.article_id}`}>
       <section className="article-card">
         <img src={article.article_img_url} alt={`${article.title} image`} />
         <p>{article.title}</p>
         <p>{article.author}</p>
         <button disabled aria-label="number of votes">
-          <i class="fa-solid fa-heart"></i>
+          <i className="fa-solid fa-heart"></i>
           {article.votes}
         </button>
         <button disabled aria-label="number of comments">
-          <i class="fa-solid fa-message"></i>
+          <i className="fa-solid fa-message"></i>
           {article.comment_count}
         </button>
       </section>
